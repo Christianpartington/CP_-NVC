c38a550a0d13915187ba42f6eb1d81c0199f0fd6
21810292 <21810292@HSPD14082887>
2019-11-04 11:38:06 +0000

    Add project files.

---
+++ MonContosoNVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MonContosoNVC5.Startup))]
+namespace MonContosoNVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
